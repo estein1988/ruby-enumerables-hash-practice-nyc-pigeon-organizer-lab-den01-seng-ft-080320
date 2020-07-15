7998fd30af5f554de786f52087c1836e15d54cbb
Elliott Stein <estein1988@gmail.com>
2020-07-15 18:14:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,6 @@ def nyc_pigeon_organizer(data)
   final_results = data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, name|
       names.each do |name|
-        binding.pry
         if !final_array[name]
           final_array[name] = {}
         end
@@ -14,7 +13,6 @@ def nyc_pigeon_organizer(data)
         final_array[name][key].push(inner_key)
       end
     end
-  final_array
   end
   binding.pry
 end
