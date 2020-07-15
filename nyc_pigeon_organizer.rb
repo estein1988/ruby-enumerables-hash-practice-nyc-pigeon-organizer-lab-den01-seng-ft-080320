fad425dea583aa76263aa073ee84575f8baf2c37
Elliott Stein <estein1988@gmail.com>
2020-07-15 18:06:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,8 +7,8 @@ def nyc_pigeon_organizer(data)
         binding.pry
         if !final_array[name]
           final_array[name] = {}
+          end
         end
-      end
       end
   final_array
   end
